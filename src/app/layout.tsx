88a4448a59b58f14a610a9bde3ced8902926c54b
Pepela John <pepelajahy@gmail.com>
2025-04-16 12:12:22 +0300

    og titiles

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Head from "next/head";
 
 
 
@@ -16,6 +17,15 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+        <Head>
+        <title>SweetTooth Ke </title>
+        <meta property="og:title" content="SweetTooth Ke" />
+        <meta property="og:description" content="Home for all things sweet" />
+        <meta property="og:image" content="https://sweettoothdemo.netlify.app/image.png" />
+        <meta property="og:url" content="https://sweettoothdemo.netlify.app/" />
+        <meta property="og:type" content="website" />
+        <meta name="twitter:card" content="summary_large_image" />
+      </Head>
       <body
        
       >
