d21833c4cd9de496810a749ab375c296a07f789c
Pepela John <pepelajahy@gmail.com>
2025-04-16 12:43:36 +0300

    image preview

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,12 +9,12 @@ export const metadata:Metadata = {
   title: 'SweetTooth Ke - Delicious cakes, pastries, and desserts in Kenya',
   description: 'SweetTooth Ke brings you the most delightful cakes, pastries, and sweets delivered fresh to your door in Nairobi.',
   openGraph: {
-    title: 'SweetTooth Ke',
+    title: 'SweetTooth Ke - Delicious cakes, pastries, and desserts in Kenya',
     description: 'SweetTooth Ke brings you the most delightful cakes, pastries, and sweets delivered fresh to your door in Nairobi.',
     url: 'https://sweettoothdemo.netlify.app/',
     images: [
       {
-        url: 'https://sweettoothdemo.netlify.app/image.png',
+        url: 'https://sweettoothdemo.netlify.app/image2.png',
         width: 1200,
         height: 630,
         alt: 'Sweet Tooth landing page',
